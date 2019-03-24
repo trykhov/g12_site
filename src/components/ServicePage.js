33738b03e24d9f4367d6cb84adddd62ae1bd943b
Try <khovct@gmail.com>
2019-03-23 23:23:58 -0700

    the service page now as the navigation bar

--- src/components/ServicePage.js
+++ src/components/ServicePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Navigation from './Navigation';
 
 class ServicePage extends React.Component {
   render() {
     return(
       <div>
-        Service Page is working
+        <Navigation />
       </div>
     );
   }
