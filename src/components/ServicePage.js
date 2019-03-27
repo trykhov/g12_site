318fb21478752c3ce08f63bbe4dcb39587ca8997
Try <khovct@gmail.com>
2019-03-27 00:07:19 -0700

    made the questionaire using less code by using map()

--- src/components/ServicePage.js
+++ src/components/ServicePage.js
@@ -11,7 +11,25 @@ class ServicePage extends React.Component {
     email: "",
     startLocation: "",
     endLocation: "",
-    weight: 0
+    weight: 0,
+    price: ""
+  };
+
+  info = ["Name", "Phone Number", "Email", "Origin", "Destination", "Weight of Load"];
+
+  generateQuestions = requestInfo => {
+    return requestInfo.map(question => {
+      return (
+        <div className="input-container">
+          <h2> {question} </h2>
+          <input
+            className="input-box"
+            type="text"
+            name={question}
+          />
+        </div>
+      )
+    })
   };
 
   checkEmpty = stateList => {};
@@ -23,74 +41,32 @@ class ServicePage extends React.Component {
         <div style={{ height: "78px" }} />
         <div className="survey">
           <form action="https://formspree.io/tryc_khov@yahoo.com" method="POST">
-            <div className="input-container">
-              <h2> Name </h2>
-              <input
-                className="input-box"
-                type="text"
-                name="Name"
-                value={this.state.contactName}
-                onChange={e => this.setState({ contactName: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <h2> Phone Number </h2>
-              <input
-                className="input-box"
-                type="text"
-                name="Phone Number"
-                value={this.state.phoneNumber}
-                onChange={e => this.setState({ phoneNumber: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <h2> Email </h2>
-              <input
-                className="input-box"
-                type="email"
-                name="Email"
-                value={this.state.email}
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <h2> Starting Location </h2>
-              <input
-                className="input-box"
-                type="text"
-                name="From"
-                value={this.state.startLocation}
-                onChange={e => this.setState({ startLocation: e.target.value })}
-              />
-            </div>
-            <div>
-              <h2> Destination </h2>
-              <input
-                className="input-box"
-                type="text"
-                name="To"
-                value={this.state.endLocation}
-                onChange={e => this.setState({ endLocation: e.target.value })}
-              />
-            </div>
-            <div className="input-container">
-              <h2> Weight of Load </h2>
-              <input
-                className="input-box"
-                type="text"
-                name="Weight"
-                value={this.state.weight}
-                onChange={e => this.setState({ weight: e.target.value })}
-              />
-            </div>
+            {this.generateQuestions(this.info)}
             <div className="input-container">
               <h2> Dry Van? </h2>
-              <input className="dry-van-yes" type="radio" name="dryVan" value="Yes" defaultChecked/>Yes
-              <input className="dry-van-no" type="radio" name="dryVan" value="No" />No
+              <input
+                className="dry-van-yes"
+                type="radio"
+                name="dryVan"
+                value="Yes"
+                defaultChecked
+              />
+              Yes
+              <input
+                className="dry-van-no"
+                type="radio"
+                name="dryVan"
+                value="No"
+              />
+              No
             </div>
             <div className="input-container">
               <h2> Price </h2>
-              <input className="input-box" type="text" name="Price" />
+              $<input
+                className="input-box"
+                type="text"
+                name="Price"
+              />
             </div>
             <input className="submit-button" type="submit" value="Send" />
           </form>
