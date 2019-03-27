3d7a2ced7f879b75b4dc2cd97d82faf2acd24fee
Try <khovct@gmail.com>
2019-03-26 20:36:02 -0700

    formatted the service page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch} from "react-router-dom";
 import Navigation from "./Navigation";
 import Intro from "./Intro";
 import About from "./About";
--- src/components/ServicePage.js
+++ src/components/ServicePage.js
@@ -8,61 +8,92 @@ class ServicePage extends React.Component {
   state = {
     contactName: "",
     phoneNumber: "",
+    email: "",
     startLocation: "",
     endLocation: "",
-    weight: 0,
-    dryVan: true
+    weight: 0
   };
+
+  checkEmpty = stateList => {};
 
   render() {
     return (
-      <section>
+      <section style={{ backgroundColor: "#10316b", height: "100%" }}>
         <Navigation />
         <div style={{ height: "78px" }} />
         <div className="survey">
-          <div className="contactName">
-            <h3> Name </h3>
-            <input
-              type="text"
-              value={this.state.contactName}
-              onChange={e => this.setState({ contactName: e.target.value })}
-            />
-          </div>
-          <div className="phoneNumber">
-            <h3> Phone Number </h3>
-            <input
-              type="text"
-              value={this.state.phoneNumber}
-              onChange={e => this.setState({ phoneNumber: e.target.value })}
-            />
-          </div>
-          <div className="locations">
-            <h3> Start </h3>
-            <input
-              type="text"
-              value={this.state.startLocation}
-              onChange={e => this.setState({ startLocation: e.target.value })}
-            />
-            <h3> Destination </h3>
-            <input
-              type="text"
-              value={this.state.endLocation}
-              onChange={e => this.setState({ endLocation: e.target.value })}
-            />
-          </div>
-          <div className="weight">
-            <h3> Weight </h3>
-            <input
-              type="text"
-              value={this.state.weight}
-              onChange={e => this.setState({ weight: e.target.value })}
-            />
-          </div>
-          <div className="dry">
-            <h3> Dry Van </h3>
-            <input type="radio" name="choice" value="yes" checked /> Yes
-            <input type="radio" name="choice" value="no" /> No
-          </div>
+          <form action="https://formspree.io/tryc_khov@yahoo.com" method="POST">
+            <div className="input-container">
+              <h2> Name </h2>
+              <input
+                className="input-box"
+                type="text"
+                name="Name"
+                value={this.state.contactName}
+                onChange={e => this.setState({ contactName: e.target.value })}
+              />
+            </div>
+            <div className="input-container">
+              <h2> Phone Number </h2>
+              <input
+                className="input-box"
+                type="text"
+                name="Phone Number"
+                value={this.state.phoneNumber}
+                onChange={e => this.setState({ phoneNumber: e.target.value })}
+              />
+            </div>
+            <div className="input-container">
+              <h2> Email </h2>
+              <input
+                className="input-box"
+                type="email"
+                name="Email"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+              />
+            </div>
+            <div className="input-container">
+              <h2> Starting Location </h2>
+              <input
+                className="input-box"
+                type="text"
+                name="From"
+                value={this.state.startLocation}
+                onChange={e => this.setState({ startLocation: e.target.value })}
+              />
+            </div>
+            <div>
+              <h2> Destination </h2>
+              <input
+                className="input-box"
+                type="text"
+                name="To"
+                value={this.state.endLocation}
+                onChange={e => this.setState({ endLocation: e.target.value })}
+              />
+            </div>
+            <div className="input-container">
+              <h2> Weight of Load </h2>
+              <input
+                className="input-box"
+                type="text"
+                name="Weight"
+                value={this.state.weight}
+                onChange={e => this.setState({ weight: e.target.value })}
+              />
+            </div>
+            <div className="input-container">
+              <h2> Dry Van? </h2>
+              <input className="dry-van-yes" type="radio" name="dryVan" value="Yes" defaultChecked/>Yes
+              <input className="dry-van-no" type="radio" name="dryVan" value="No" />No
+            </div>
+            <div className="input-container">
+              <h2> Price </h2>
+              <input className="input-box" type="text" name="Price" />
+            </div>
+            <input className="submit-button" type="submit" value="Send" />
+          </form>
         </div>
       </section>
     );
